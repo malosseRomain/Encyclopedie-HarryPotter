5a10922aa499cd15faeeb4d2f96d6fa88b34fa35
malosseRomain <romain.malosse43@icloud.com>
2024-02-20 18:57:07 +0100

    SaMarcheTjrPas

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
+// fichier router/index.js ou un nom similaire
 import { createRouter, createWebHistory } from 'vue-router';
-import Accueil from '../components/Accueil.vue';
-import Potions from '../components/Potions.vue';
+import PotionsPage from '../components/Potions.vue'; // Assurez-vous que le chemin est correct
 
 const routes = [
   {
     path: '/',
-    name: 'accueil',
-    component: Accueil,
+    name: 'Acceuil',
+    component: Home
   },
   {
     path: '/potions',
     name: 'potions',
-    component: Potions,
+    component: PotionsPage,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
 
