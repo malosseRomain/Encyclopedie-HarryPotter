4aed579d5ed1ef02aa64dbf42b092359ba56b1de
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2024-03-08 09:12:06 +0100

    annulation proxy

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const { URL_ORIGIN } = process.env;
-
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    // //Permet d'assurer que l'accès direct à des routes renvoie à l'accueil pour faire fonctionner le routeur
-    // proxy: {
-    //   '/': {
-    //     target: "http://localhost:5173",
-    //     rewrite: (path) => path === '/' ? '/index.html' : path
-    //   }
-    // }
-  }
+  plugins: [vue()]
 });
