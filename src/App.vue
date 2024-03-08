4c3b31d0e1ef99e10b315224d6301be529746ea0
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2024-03-08 10:08:17 +0100

    header avec liens

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,71 @@
 <script setup>
+import { ref } from 'vue'
+
+const liens = ref([
+  {title : 'Potions', route : '/Potions'},
+  {title : 'Sorts', route : '/Sorts'},
+  {title : 'Livres', route : '/Livres'},
+  {title : 'Personnages', route : '/Personnages'}
+])
+
+const accueil = ref({title : 'Accueil', route : '/'})
 
 </script>
 
 <template>
+  <header>
+    <div><router-link :to='accueil.route'>{{ accueil.title }}</router-link></div>
+
+    <ul>
+      <li v-for="(lien, index) in liens" :key="index">
+        <router-link :to='lien.route'>{{ lien.title }}</router-link>
+      </li>
+    </ul>
+  </header>
+
   <router-view/>
 </template>
 
-<style scoped></style>
+<style scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 2rem;
+  background-color: var(--header-background, #cfc8c8);
+}
+
+header div,
+header ul {
+  display: flex;
+  align-items: center;
+}
+
+header div {
+  margin-right: auto;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+}
+
+ul li {
+  margin-left: 1rem;
+}
+
+a {
+  color: var(--link-color, #646cff);
+  text-decoration: none;
+  transition: color 0.3s;
+}
+
+a:hover {
+  color: var(--link-hover-color, #535bf2);
+}
+</style>
