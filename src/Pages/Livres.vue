f2a2cca23412ad8f02d12130b57d28c4133dba70
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2024-03-24 13:32:32 +0100

    separation component livre

--- src/Pages/Livres.vue
+++ src/Pages/Livres.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import livreComponent from '../components/livreComponent.vue';
 import { getLivres } from '../controllers/livresController';
 
 const list = ref([])
@@ -14,52 +15,29 @@ const fetchData = async () => {
   }
 }
 
-
 onMounted(fetchData)
 </script>
 
 <template>
   <div v-if="erreur !== 0">{{ erreur }}</div>
-  <div v-else>
-    <div class="container">
-      <h1> Livres </h1>
-      <div class="books-list">
-        <div v-for="item in list" :key="item.id" class="books-item">
-          <img v-if="item.attributes.cover" :src="item.attributes.cover" alt="Image du livre" />
-          <div class="books-details">
-            <p v-if="item.attributes.title" class="item-details padding"><span>Nom : </span>{{ item.attributes.title }}</p>
-            <p v-if="item.attributes.pages" class="item-details padding"><span>Nombre de page : </span>{{item.attributes.pages }}</p>
-            <p v-if="item.attributes.release_date" class="item-details padding"><span>Date de sortie : </span>{{item.attributes.release_date }}</p>
-            <p v-if="item.attributes.author" class="item-details padding"><span>Créateur : </span>{{item.attributes.author }}</p>
-            <p v-if="item.attributes.summary" class="PrettyDisplay padding"><span>Résumé :</span> {{item.attributes.summary }}</p>
-          </div>
-          <a :href="item.attributes.wiki">
-            <p class="Wiki">En savoir plus avec le wiki</p>
-          </a>
-        </div>
-      </div>
-    </div>
-  </div>
+  <section v-else>
+    <h1> Livres </h1>
+    <ul>
+      <livreComponent v-for="item in list" :key="item.id" :item="item" :msg="item" class="books-item" />
+    </ul>
+  </section>
 </template>
 
 
 <style scoped>
-.container {
+section {
   padding: 20px;
   text-align: center;
 }
 
-span{
-  font-weight: bold;
-}
-
-h1 {
-  color: #000000;
-}
-
-.books-list {
+ul {
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(450px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
   grid-gap: 30px;
   justify-items: center;
 }
@@ -68,47 +46,14 @@ h1 {
   background-color: rgb(241, 237, 237);
   width: 100%;
   transition: transform 0.3s ease-in-out;
+
+  &:hover {
+    transform: scale(1.02);
+  }
 }
 
-.item-details {
-  border-bottom: 3px solid #000000;
-}
 
-.books-item:hover {
-  transform: scale(1.02);
-}
-
-.padding {
-  padding-bottom: 20px;
-  padding-right: 20px;
-  padding-left: 20px;
-}
-
-.books-item img {
-  width: 100%;
-  height: auto;
-}
-
-.books-details {
-  border-bottom: 1px solid #ccc;
-  height: 400px;
-  color: black;
-}
-
-a {
-  text-decoration: none;
-  color: white;
-}
-
-.PrettyDisplay {
-  text-align: justify;
-  overflow: auto;
-  height: 32%;
-}
-
-.Wiki {
-  background-color: rgba(0, 123, 255, 1);
-  padding: 15px;
-  margin: 0;
+h1 {
+  color: #000000;
 }
 </style>
---
+++ src/components/livreComponent.vue
@@ -0,0 +1,88 @@
+<script setup>
+const props = defineProps({
+    item: {
+        type: Object,
+        required: true
+    }
+});
+</script>
+
+<template>
+    <div class="books-item">
+        <img v-if="item.attributes.cover" :src="item.attributes.cover" alt="Image du livre" />
+        <div id="books-details">
+            <p>
+                <strong>Nom :</strong> {{ item.attributes.title || "N/A" }}
+            </p>
+            <p>
+                <strong>Nombre de page :</strong> {{ item.attributes.pages || "N/A" }}
+            </p>
+            <p>
+                <strong>Date de sortie :</strong> {{ new Date(item.attributes.release_date).toLocaleDateString('fr-FR')
+            || "N/A" }}
+            </p>
+            <p>
+                <strong>Créateur :</strong> {{ item.attributes.author || "N/A" }}
+            </p>
+            <p id="resume">
+                <strong>Résumé :</strong> {{ item.attributes.summary || "N/A" }}
+            </p>
+        </div>
+        <a :href="item.attributes.wiki">
+            <p id="Wiki">En savoir plus avec le wiki</p>
+        </a>
+    </div>
+</template>
+
+
+<style scoped>
+.books-item {
+    background-color: rgb(241, 237, 237);
+    width: 100%;
+    border: 10px double #834545;
+    border-radius: 25px;
+    transition: transform 0.3s ease-in-out;
+}
+
+
+#books-details {
+    border-bottom: 1px solid #ccc;
+    height: 400px;
+    color: black;
+
+    p {
+        border-bottom: 3px solid #000000;
+        padding-bottom: 20px;
+        padding-right: 20px;
+        padding-left: 20px;
+    }
+}
+
+img {
+    width: 100%;
+    height: auto;
+    border-radius: 15px;
+}
+
+#resume {
+    padding-bottom: 20px;
+    padding-right: 20px;
+    padding-left: 20px;
+    text-align: justify;
+    overflow: auto;
+    height: 32%;
+}
+
+
+a {
+    text-decoration: none;
+    color: white;
+
+    #Wiki {
+        background-color: rgba(0, 123, 255, 1);
+        padding: 15px;
+        margin: 0;
+        border-radius: 0 0 15px 15px;
+    }
+}
+</style>
