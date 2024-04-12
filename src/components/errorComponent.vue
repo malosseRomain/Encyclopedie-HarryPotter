2df0052c69d15248821c443db9b0bee38a46c596
Bilal Oulahal <Bilal.OULAHAL@etu.uca.fr>
2024-04-12 22:45:46 +0200

    ajout errorComponent

---
+++ src/components/errorComponent.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+    reloadPage: {
+        type: Function,
+        required: true
+    },
+    msgError: {
+        type: String,
+        required: true
+    }
+});
+</script>
+
+<template>
+      <div class="no-results-message">
+        {{msgError}}. Cliquez <a href="#" @click="reloadPage">ici</a> pour recharger la page.
+      </div>
+</template>
+
+<style scoped>
+.no-results-message {
+  text-align: center;
+  font-size: 24px;
+  color: red;
+  background-color: #f8d7da;
+  border: 2px solid #f5c6cb; 
+  border-radius: 5px;
+  padding: 20px; 
+  margin-top: 20px; 
+}
+</style>
